e362efe32c1f759eeca3a89abdd6049d3fc1e409
troy896 <troy896@gmail.com>
2026-02-17 13:40:48 -0600

    Refactor setup.sh for improved readability and structure

--- setup.sh
+++ setup.sh
@@ -60,7 +60,7 @@ setup_kernelsu() {
 
 	# Add entries in Makefile and Kconfig if not already existing
     grep -q "kernelsu" "$DRIVER_MAKEFILE" || printf "\nobj-\$(CONFIG_KSU) += kernelsu/\n" >> "$DRIVER_MAKEFILE" && echo "[+] Modified Makefile."
-    grep -q "source \"drivers/kernelsu/Kconfig\"" "$DRIVER_KCONFIG" || sed -i "/endmenu/i\source \"drivers/kernelsu/Kconfig\"" "$DRIVER_KCONFIG" && echo "[+] Modified Kconfig."
+	grep -q "source \"drivers/kernelsu/Kconfig\"" "$DRIVER_KCONFIG" || sed -i "/endmenu/i\source \"drivers/kernelsu/Kconfig\"" "$DRIVER_KCONFIG" && echo "[+] Modified Kconfig."
 	echo '[+] Done.'
 }
 
