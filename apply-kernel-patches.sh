ded322643cc2c946bba6f49f1ef607485ad5379f
troy896 <troy896@gmail.com>
2026-04-16 09:18:45 -0500

    Update apply-kernel-patches.sh

--- apply-kernel-patches.sh
+++ apply-kernel-patches.sh
@@ -41,8 +41,6 @@ else
 fi
 
 apply "$COMMON/minimise_wakeup_time.patch"
-apply "$COMMON/int_sqrt.patch"
-apply "$COMMON/force_tcp_nodelay.patch"
 apply "$COMMON/reduce_gc_thread_sleep_time.patch"
 apply "$COMMON/add_timeout_wakelocks_globally.patch"
 apply "$COMMON/f2fs_reduce_congestion.patch"
@@ -53,24 +51,6 @@ if [ "$MAJOR" -ge 6 ]; then
 else
   sed 's/SYM_FUNC_START_PI(clear_page)/SYM_FUNC_START_PI(__pi_clear_page)/' \
     "$COMMON/clear_page_16bytes_align.patch" | patch -p1 -F3 --forward || true
-fi
-
-apply "$COMMON/adjust_cpu_scan_order.patch"
-apply "$COMMON/avoid_extra_s2idle_wake_attempts.patch"
-apply "$COMMON/disable_cache_hot_buddy.patch"
-apply "$COMMON/f2fs_enlarge_min_fsync_blocks.patch"
-apply "$COMMON/increase_ext4_default_commit_age.patch"
-apply "$COMMON/increase_sk_mem_packets.patch"
-apply "$COMMON/reduce_pci_pme_wakeups.patch"
-apply "$COMMON/silence_irq_cpu_logspam.patch"
-apply "$COMMON/silence_system_logspam.patch"
-
-if [ -f "$COMMON/unicode_bypass_fix_6.1+.patch" ]; then
-  if [ "$MAJOR" -gt 6 ] || { [ "$MAJOR" -eq 6 ] && [ "$MINOR" -ge 1 ]; }; then
-    apply "$COMMON/unicode_bypass_fix_6.1+.patch"
-  else
-    apply "$COMMON/unicode_bypass_fix_6.1-.patch"
-  fi
 fi
 
 [ -f "$COMMON/IPv6_NAT_FIX.patch" ] && apply "$COMMON/IPv6_NAT_FIX.patch"
